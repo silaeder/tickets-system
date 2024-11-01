949d4c7f6454053fc5426378073261c880817e2b
Vladimir Papazov <papazovvladimir88@gmail.com>
2024-11-01 19:28:08 +0300

    email util fix

--- src/app/utils/email.ts
+++ src/app/utils/email.ts
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || '587'),
   secure: true,
+  from: process.env.SMTP_FROM,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
